6b19a15d95dbc594145262d4aea3cb4e4043c42a
ldplatter <139258439+ldplatter@users.noreply.github.com>
2023-07-19 10:15:21 -0400

    Add files via upload

---
+++ prune_tree.py
@@ -0,0 +1,36 @@
+from Bio import Phylo
+
+master_tree = Phylo.read("202_taxa_set_tree.nwk", "newick")
+
+
+def format_taxa_name(taxa_name):
+    taxa_name = taxa_name.lower()
+    taxa_name = taxa_name.replace(" ", "_")
+    taxa_name = taxa_name.capitalize()
+    return(taxa_name)
+
+
+shared_taxa_names = []
+with open("shared_taxa.csv", "r") as file:
+    reader = file.readlines()
+    for row in reader:
+        shared_taxa_names += [format_taxa_name(row[:-1])]
+    
+
+all_taxa = []
+for clade in master_tree.find_clades():
+    if clade.is_terminal():
+        all_taxa += [format_taxa_name(clade.name)]
+        
+
+taxa_to_prune = []
+for taxa in all_taxa:
+    if not(taxa in shared_taxa_names):
+        taxa_to_prune += [taxa]
+        
+
+for taxa in taxa_to_prune:
+    master_tree.prune(taxa)
+Phylo.write(master_tree, "shared_taxa_tree.nwk", "newick")
+
+ 
