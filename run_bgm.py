2101f4a29fe35da28cd272b848593009c3c5d923
Moran Guo <mguo17@u.rochester.edu>
2023-07-27 14:59:57 +0800

    Updated run_bgm.py

--- run_bgm.py
+++ run_bgm.py
@@ -77,95 +77,103 @@ def align_fasta(fastafile, outputfile, threads=5):
     assert os.path.exists(outputfile), f"Output file {outputfile} was not created."
 
 
-def call_bgm(fasta_file, tree, model):
+def call_bgm(f_file, tree, model):
     cmd = f"hyphy /usr/local/share/hyphy/TemplateBatchFiles/BGM.bf --type amino-acid " \
-          f"--alignment {fasta_file} " \
+          f"--alignment {f_file} " \
           f"--tree {tree} --baseline_model {model}"
     subprocess.call(cmd, shell=True)
 
 
-# Takes two command line arguments, one for each protein
-protein1 = sys.argv[1]
-protein2 = sys.argv[2]
+def process_files(p1, p2, tree):
+    # Output file names
+    aligned_protein1 = f"aligned_{os.path.basename(p1)}"
+    aligned_protein2 = f"aligned_{os.path.basename(p2)}"
 
-# Output file names
-aligned_protein1 = f"aligned_{protein1}"
-aligned_protein2 = f"aligned_{protein2}"
+    # Align the sequences
+    align_fasta(p1, aligned_protein1)
+    align_fasta(p2, aligned_protein2)
 
-# Align the sequences
-align_fasta(protein1, aligned_protein1)
-align_fasta(protein2, aligned_protein2)
+    Protein1_fasta = open(aligned_protein1, "r").readlines()
+    Protein2_fasta = open(aligned_protein2, "r").readlines()
+    Protein1_dict = make_seq_dict(Protein1_fasta)
+    Protein2_dict = make_seq_dict(Protein2_fasta)
 
-Protein1_fasta = open(aligned_protein1, "r").readlines()
-Protein2_fasta = open(aligned_protein2, "r").readlines()
+    species_list = [i for i in Protein1_dict.keys() if i in Protein2_dict.keys()]
 
-# Load the master tree, allowing for two phylo master tree options
-if len(sys.argv) > 3 and sys.argv[3] == "108":
-    master_tree = Phylo.read("finished_mam_timetree.nwk", "newick")
-else:
-    master_tree = Phylo.read("202_taxa_set_tree.nwk", "newick")
+    concat_dict = {}
 
-Protein1_dict = make_seq_dict(Protein1_fasta)
-Protein2_dict = make_seq_dict(Protein2_fasta)
+    for i in species_list:
+        concat_dict[i] = Protein1_dict[i] + Protein2_dict[i]
 
-species_list = [i for i in Protein1_dict.keys() if i in Protein2_dict.keys()]
+    out = ""
+    taxa_list = ""
 
-concat_dict = {}
-
-for i in species_list:
-    concat_dict[i] = Protein1_dict[i] + Protein2_dict[i]
-
-out = ""
-taxa_list = ""
-for i in concat_dict.keys():
-    out += ">"
-    out += i
-    out += "\n"
-    out += split_by_60(concat_dict[i])
-    taxa_list += i.replace("_", " ")
-    taxa_list += "\n"
+    for i in concat_dict.keys():
+        out += ">"
+        out += i
+        out += "\n"
+        out += split_by_60(concat_dict[i])
+        taxa_list += i.replace("_", " ")
+        taxa_list += "\n"
 
 # New, Get the base names of the input fasta files (without the '.fa' extension)
-base_name1 = os.path.splitext(protein1)[0]
-base_name2 = os.path.splitext(protein2)[0]
+    base_name1 = os.path.splitext(os.path.basename(p1))[0]
+    base_name2 = os.path.splitext(os.path.basename(p2))[0]
 
-concat_fasta = f"{base_name1}_{base_name2}.fa"
-csv = f"{base_name1}_{base_name2}_taxa_list.csv"
+    concat_fasta = f"{base_name1}_{base_name2}.fa"
+    csv = f"{base_name1}_{base_name2}_taxa_list.csv"
 
-with open(concat_fasta, "w") as f:
-    f.write(out)
+    with open(concat_fasta, "w") as f:
+        f.write(out)
 
-with open(csv, "w") as l:
-    l.write(taxa_list)
+    with open(csv, "w") as l:
+        l.write(taxa_list)
 
 # Read the taxa names from the output csv file
-shared_taxa_names = []
-with open(csv, "r") as file:
-    reader = file.readlines()
-    for row in reader:
-        shared_taxa_names += [format_taxa_name(row[:-1])]
+    shared_taxa_names = []
+    with open(csv, "r") as file:
+        reader = file.readlines()
+        for row in reader:
+            shared_taxa_names += [format_taxa_name(row[:-1])]
 
-all_taxa = []
-for clade in master_tree.find_clades():
-    if clade.is_terminal():
-        all_taxa += [format_taxa_name(clade.name)]
+    all_taxa = []
+    for clade in tree.find_clades():
+        if clade.is_terminal():
+            all_taxa += [format_taxa_name(clade.name)]
 
-taxa_to_prune = []
-for taxa in all_taxa:
-    if not (taxa in shared_taxa_names):
-        taxa_to_prune += [taxa]
+    taxa_to_prune = []
+    for taxa in all_taxa:
+        if not (taxa in shared_taxa_names):
+            taxa_to_prune += [taxa]
+
+    for taxa in taxa_to_prune:
+        if len(sys.argv) > 3 and sys.argv[3] == "108":
+            taxa = taxa.upper()
+            tree.prune(taxa)
+        else:
+            tree.prune(taxa)
+
+    shared_phylo_tree = f"shared_taxa_{base_name1}_{base_name2}.nwk"
+
+    Phylo.write(tree, shared_phylo_tree, "newick")
+
+    sub_model = "GTR"
+    call_bgm(concat_fasta, shared_phylo_tree, sub_model)
 
 
-for taxa in taxa_to_prune:
+if __name__ == "__main__":
+    protein1 = sys.argv[1]
+    protein2 = sys.argv[2]
+# Load the master tree, allowing for two phylo master tree options
     if len(sys.argv) > 3 and sys.argv[3] == "108":
-        taxa = taxa.upper()
-        master_tree.prune(taxa)
+        master_tree = Phylo.read("finished_mam_timetree.nwk", "newick")
     else:
-        master_tree.prune(taxa)
+        master_tree = Phylo.read("202_taxa_set_tree.nwk", "newick")
 
-shared_phylo_tree = f"shared_taxa_{base_name1}_{base_name2}.nwk"
-
-Phylo.write(master_tree, shared_phylo_tree, "newick")
-
-sub_model = "GTR"
-call_bgm(concat_fasta, shared_phylo_tree, sub_model)
+    if os.path.isdir(protein2):
+        for filename in os.listdir(protein2):
+            if filename.endswith('.fa') or filename.endswith('.fasta'):
+                fasta_file = os.path.join(protein2, filename)
+                process_files(protein1, fasta_file, master_tree)
+    else:
+        process_files(protein1, protein2, master_tree)
