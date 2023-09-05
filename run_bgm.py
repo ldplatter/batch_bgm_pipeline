d339af961603998c26a6125589c7e8aa71f85b06
Moran Guo <mguo17@u.rochester.edu>
2023-09-05 00:01:23 -0400

    Merged the run_bgm.py with the post-processing json file part, along with the network. Removed parallelization.

--- run_bgm.py
+++ run_bgm.py
@@ -1,10 +1,13 @@
 import os
 import sys
 import subprocess
-from Bio import Phylo
-from concurrent.futures import ThreadPoolExecutor
+from Bio import Phylo, SeqIO
 import glob
 import pexpect
+import pandas as pd
+import json
+from networkx import *
+import matplotlib.pyplot as plt
 
 '''
 The structure of the command is: "python3 run_bgm.py protein1.fa protein2.fa [taxa_number/NA]".
@@ -15,14 +18,20 @@ concatenated with protein1 and run BGM tests respectively.
 This modified script takes two user-defined raw protein fasta files as input, outputs the aligned fasta file,
 both single and concatenated, the shared taxa list as a CSV file, and the pruned phylogenetic tree Newick file.
 If taxa number equals to 108, then the 108 mammalian taxa phylogenetic tree was used to determine the final output tree.
-
 If this argument is ignored, then the new 202 taxa master tree was used.
 
 The script then carry out BGM utilizing HyPhy package automatically. It creates a HyPhy interactive session and 
 automatically run BGM tests using GTR model and input data (generated prior to running BGMs).
 
 The substitution model and further parameters could be also modified by editing parameters inside the call_bgm function.
+
+Lastly, the script outputs summary statistics of the BGM test results and the network topology based on the input data,
+using the networkx package.
 '''
+batch = False
+batch_directory = ""
+make_graph = True  # Default: True
+protein1_length = 0
 
 
 def format_taxa_name(taxa_name):
@@ -84,7 +93,7 @@ def align_fasta(fastafile, outputfile, threads=5):
 def call_bgm(f_file, tree):
     command = "hyphy -i"
     child = pexpect.spawn(command)
-    child.sendline("4")  # Coevolution
+    child.sendline("4")  # Co-evolution
     child.sendline("1")  # BGM
     child.sendline("2")  # Amino-Acid
     child.sendline(f_file)
@@ -99,9 +108,41 @@ def call_bgm(f_file, tree):
     child.interact()
 
 
+def write_summary_and_csv(bgm_df, protein_length, threshold, base_name1, base_name2, directory=None):
+    # Write the summary files to multiple csv files and move them to new directories
+    probable_sites = find_probable_sites(bgm_df, threshold)
+    labeled_sites = add_within_between(probable_sites, protein_length)
+    csv_name = f"{base_name1}_{base_name2}_{int(threshold * 100)}_probable_sites.csv"
+    labeled_sites.to_csv(csv_name)
+
+    density, directionality_for, directionality_rev = generate_summary(labeled_sites)
+    summary_name = f"{base_name1}_{base_name2}_summary.txt"
+    with open(summary_name, "w") as summary_file:
+        # Write the nutshell information between target proteins.
+        summary_file.write("Proportion of interactions between {} that are in-between: {}\n\n"
+                           .format(f"{base_name1}_{base_name2}", density))
+        summary_file.write("Percentage of directional interactions from {} to its protein partner (direction of "
+                           "conditional dependence): {}%\n\n"
+                           .format(base_name1, round(directionality_for, 5) * 100))
+        summary_file.write("Percentage of directional interactions from protein partner to {} (direction of "
+                           "conditional dependence): {}%\n\n"
+                           .format(base_name1, round(directionality_rev, 5) * 100))
+
+    if directory:
+        if not os.path.exists(f"{directory}/{base_name1}_{base_name2}"):
+            make_dir = f"mkdir {directory}/{base_name1}_{base_name2}"
+            subprocess.call(make_dir, shell=True)
+
+        cmd_csv = f"mv {csv_name} {directory}/{base_name1}_{base_name2}"
+        cmd_summary = f"mv {summary_name} {directory}/{base_name1}_{base_name2}"
+        subprocess.call(cmd_csv, shell=True)
+        subprocess.call(cmd_summary, shell=True)
+
+
 def process_files(p1, p2, tree):
-    # Output file names
-    aligned_protein1 = f"aligned_{os.path.basename(p1)}" # if the protein is already aligned, delete the 'aligned_' part
+    # Output file names, change to "aligned_{os.path.basename(p1)}" if using the align_fasta function.
+    global bgm_file_path
+    aligned_protein1 = f"{os.path.basename(p1)}"
     aligned_protein2 = os.path.join(os.path.dirname(p2), f"{os.path.basename(p2)}")
 
     protein1_fasta = open(aligned_protein1, "r").readlines()
@@ -137,8 +178,8 @@ def process_files(p1, p2, tree):
     with open(concat_fasta, "w") as f:
         f.write(out)
 
-    with open(csv, "w") as lst:
-        lst.write(taxa_list)
+    with open(csv, "w") as l:
+        l.write(taxa_list)
 
     # Read the taxa names from the output csv file
     shared_taxa_names = []
@@ -166,20 +207,162 @@ def process_files(p1, p2, tree):
 
     shared_phylo_tree = f"shared_taxa_{base_name1}_{base_name2}.nwk"
     Phylo.write(tree, shared_phylo_tree, "newick")
-    return concat_fasta, shared_phylo_tree
-    # call_bgm(concat_fasta, shared_phylo_tree)
+
+    base_dir = f"{batch_directory}/" if batch else ""
+
+    # Calculate BGM
+    if batch:
+        # Building full paths
+        concat_fasta_path = os.path.join(concat_fasta)
+        shared_phylo_tree_path = os.path.join(shared_phylo_tree)
+        bgm_file_path = os.path.join(base_dir, f"{concat_fasta}.BGM.json")
+
+        call_bgm(concat_fasta_path, shared_phylo_tree_path)
+        cmd = f"mv {concat_fasta}.BGM.json {base_dir}"
+        subprocess.call(cmd, shell=True)
+    else:
+        call_bgm(concat_fasta, shared_phylo_tree)
+
+    bgm_df = initialize_results(bgm_file_path)
+
+    # Process for both 0.9 and 0.5 threshold values
+    for threshold in [0.9, 0.5]:
+        write_summary_and_csv(bgm_df, protein1_length, threshold, base_name1, base_name2, base_dir)
+
+    if make_graph:
+        create_graph(bgm_df, 0.9)
+
+        if batch:
+            cmd_connected = f"mv {protein1}_{protein2}_connected_nodes.png {base_dir}/"
+            cmd_all = f"mv {protein1}_{protein2}_all_nodes.png {base_dir}/"
+            subprocess.call(cmd_connected, shell=True)
+            subprocess.call(cmd_all, shell=True)
 
 
-# New function, run in multiple parallel processes
-def run_in_parallel(params):
-    f_file, tree = params
-    call_bgm(f_file, tree)
+# Post BGM file processing is below:
+def length_of_first_sequence(fasta_file):
+    for record in SeqIO.parse(fasta_file, "fasta"):
+        return len(record.seq)
+
+
+def initialize_results(file):
+    f = open(file)
+    data = json.load(f)
+
+    site1_list = []
+    site2_list = []
+    site1_to_site2_list = []
+    site2_to_site1_list = []
+    site1_or_site2_list = []
+    site1_subs_list = []
+    site2_subs_list = []
+    shared_subs_list = []
+
+    for i in data["MLE"]["content"]:
+        site1_list += [i[0]]
+        site2_list += [i[1]]
+        site1_to_site2_list += [i[2]]
+        site2_to_site1_list += [i[3]]
+        site1_or_site2_list += [i[4]]
+        site1_subs_list += [i[5]]
+        site2_subs_list += [i[6]]
+        shared_subs_list += [i[7]]
+
+    bgm_df = pd.DataFrame(
+        columns=["Site1", "Site2", "Site1 --> Site2", "Site1 <-- Site2", "Site1 <--> Site2", "Site1 Subs", "Site2 Subs",
+                 "Shared Subs"])
+
+    bgm_df["Site1"] = site1_list
+    bgm_df["Site2"] = site2_list
+    bgm_df["Site1 --> Site2"] = site1_to_site2_list
+    bgm_df["Site1 <-- Site2"] = site2_to_site1_list
+    bgm_df["Site1 <--> Site2"] = site1_or_site2_list
+    bgm_df["Site1 Subs"] = site1_subs_list
+    bgm_df["Site2 Subs"] = site2_subs_list
+    bgm_df["Shared Subs"] = shared_subs_list
+    return bgm_df
+
+
+def find_probable_sites(bgm_df, confidence=0.9):
+    probable_sites_df = bgm_df[bgm_df["Site1 <--> Site2"] >= confidence]
+    return probable_sites_df
+
+
+# Creates graphs displaying bgm data.
+def create_graph(bgm_df, confidence):
+    prob_sites = find_probable_sites(bgm_df, confidence)
+    g = Graph()
+    g2 = Graph()
+    nodes_added = []
+    for i in range(0, len(prob_sites)):
+        g.add_edge(int(prob_sites.iloc[i]["Site1"]), int(prob_sites.iloc[i]["Site2"]))
+        g2.add_edge(int(prob_sites.iloc[i]["Site1"]), int(prob_sites.iloc[i]["Site2"]))
+        if not (prob_sites.iloc[i]["Site1"] in nodes_added):
+            nodes_added += [prob_sites.iloc[i]["Site1"]]
+        if not (prob_sites.iloc[i]["Site2"] in nodes_added):
+            nodes_added += [prob_sites.iloc[i]["Site2"]]
+    for i in range(1, int(bgm_df["Site1"][bgm_df.index.values[-1]]) + 1):
+        if not (i in nodes_added):
+            g.add_node(i)
+
+    nodes_with_edges = [node for node, degree in g.degree() if degree > 0]
+    nodes_with_multi_edges = [node for node, degree in g.degree() if degree > 1]
+    node_colors = ["red" if node in nodes_with_edges else "lightblue" if node not in nodes_with_multi_edges else "red"
+                   for node in g.nodes()]
+
+    pos = spring_layout(g, k=.3, scale=.1, center=(1, 1), iterations=20)
+    pos_nodes_with_edges = {node: position for (node, position) in pos.items() if node in nodes_with_edges}
+
+    draw_networkx(g2, pos=pos_nodes_with_edges, node_color="red", node_size=150, font_size=6)
+    plt.savefig(f"{protein1}_{protein2}_connected_nodes.png")
+
+    draw_networkx(g, pos=pos, node_color=node_colors, node_size=150, font_size=6)
+    plt.savefig(f"{protein1}_{protein2}_all_nodes.png")
+
+
+# Determines if interactions are within one protein or between the two proteins, as well as determines direction of
+# interaction.
+def add_within_between(bgm_data, protein1_length):
+    # Create a copy of the dataframe to avoid modifying the original dataframe/view
+    bgm_data_copy = bgm_data.copy()
+
+    interaction_types = []
+    direction_types = []
+
+    for entry_ID in bgm_data_copy.index:
+        print()
+        if int(bgm_data_copy["Site1"][entry_ID]) <= protein1_length < int(bgm_data_copy["Site2"][entry_ID]):
+            interaction_types.append("Between")
+        else:
+            interaction_types.append("Within")
+
+        if (float(bgm_data_copy["Site1 --> Site2"][entry_ID]) / float(
+                bgm_data_copy["Site1 <--> Site2"][entry_ID])) > 0.8:
+            direction_types.append("1->2")
+        elif (float(bgm_data_copy["Site1 <-- Site2"][entry_ID]) / float(
+                bgm_data_copy["Site1 <--> Site2"][entry_ID])) > 0.8:
+            direction_types.append("2->1")
+        else:
+            direction_types.append("Bi")
+
+    bgm_data_copy["Identity"] = interaction_types
+    bgm_data_copy["Direction"] = direction_types
+
+    return bgm_data_copy
+
+
+# Generates summary data: Density and Directionality
+def generate_summary(bgm_data):
+    density = (len(bgm_data[bgm_data["Identity"] == "Between"]) / protein1_length)
+    directionality_forward = (len(bgm_data[bgm_data["Direction"] == "1->2"]) / len(bgm_data))
+    directionality_reverse = (len(bgm_data[bgm_data["Direction"] == "2->1"]) / len(bgm_data))
+    return density, directionality_forward, directionality_reverse
 
 
 if __name__ == "__main__":
     protein1 = sys.argv[1]
     protein2 = sys.argv[2]
-    input_files = []  # An empty list used to store file (names) for BGM
+    protein1_length = length_of_first_sequence(protein1)
     # Load the master tree, allowing for two phylo master tree options
     if len(sys.argv) > 3 and sys.argv[3] == "108":
         master_tree = Phylo.read("finished_mam_timetree.nwk", "newick")
@@ -187,15 +370,12 @@ if __name__ == "__main__":
         master_tree = Phylo.read("202_taxa_set_tree.nwk", "newick")
 
     if os.path.isdir(protein2):
+        batch = True
+        batch_directory = protein2
         for filename in os.listdir(protein2):
             protein2_files = glob.glob(os.path.join(protein2, '*.fa*'))
             for protein2 in protein2_files:
-                concat_fasta, shared_phylo_tree = process_files(protein1, protein2, master_tree)
-                input_files.append((concat_fasta, shared_phylo_tree))
+                # Make a deep copy of the master tree for each protein2
+                process_files(protein1, protein2, master_tree)
     else:
-        concat_fasta, shared_phylo_tree = process_files(protein1, protein2, master_tree)
-        input_files.append((concat_fasta, shared_phylo_tree))
-
-        # Use a ThreadPoolExecutor to run in parallel
-    with ThreadPoolExecutor(max_workers=8) as executor:
-        executor.map(run_in_parallel, input_files)
+        process_files(protein1, protein2, master_tree)
